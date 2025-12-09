1f3257479a131768cf93002062f41c7c577b3665
kihus <wintergamesk@gmail.com>
2025-12-09 13:43:03 -0300

    Add project files.

---
+++ FlightTickets.Models/Ticket.cs
@@ -0,0 +1,19 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace FlightTickets.Models;
+
+public class Ticket(
+    string passengerName,
+    string flightNumber,
+    string seatNumber,
+    decimal price
+    )
+{
+    [BsonId]
+    public ObjectId Id { get; private set; }
+    public string PassengerName { get; private set; } = passengerName;
+    public string FlightNumber { get; private set; } = flightNumber;
+    public string SeatNumber { get; private set; } = seatNumber;
+    public decimal Price { get; private set; } = price;
+}
